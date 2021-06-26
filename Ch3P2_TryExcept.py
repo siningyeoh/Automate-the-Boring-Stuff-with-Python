730f5f8299605eb1f98b52c5679ff8a92e72d91d
siningyeoh <75380967+siningyeoh@users.noreply.github.com>
2021-06-26 22:58:27 +0100

    Update Ch3P2_TryExcept.py

--- Ch3P2_TryExcept.py
+++ Ch3P2_TryExcept.py
@@ -1,11 +1,14 @@
 def collatz(number):
     try:
-        if int(number)%2==0:
-            print (int(number)//2)
-        if int(number)%2==1:
-            print (3*int(number)+1)
-    except ValueError:
-        print ('Please type an INTEGER.')
+        print (number)
+        while int(abs(number))>1:
+            if int(number)%2==0:
+                number=int(number)//2
+            elif int(number)%2==1:
+                number=3*int(number)+1
+            print (number)
+    except:
+        print ('Please give us a POSITIVE INTEGER.')
         
 print ('Please type an integer.')
 collatz(input())
