b1c0f02c1836d456b4641a8f53ee24b3441b7646
siningyeoh <75380967+siningyeoh@users.noreply.github.com>
2021-06-26 22:52:20 +0100

    Update Ch3P1_Collatz.py

--- Ch3P1_Collatz.py
+++ Ch3P1_Collatz.py
@@ -1,8 +1,11 @@
 def collatz(number):
-    if int(number)%2==0:
-        print (int(number)//2)
-    if int(number)%2==1:
-        print (3*int(number)+1)
+    print (number)
+    while int(number)>1:
+        if int(number)%2==0:
+            number=int(number)//2
+        elif int(number)%2==1:
+            number=3*int(number)+1
+        print (number)
 
 print ('Please type a number.')
 collatz(input())
