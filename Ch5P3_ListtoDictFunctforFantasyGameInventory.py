a4ab20df32a5e5b2aff4301c6b2ae9c1a2c23a2b
siningyeoh <75380967+siningyeoh@users.noreply.github.com>
2021-08-13 16:43:18 +0100

    Add files via upload

---
+++ Ch5P3_ListtoDictFunctforFantasyGameInventory.py
@@ -0,0 +1,14 @@
+def addToInventory(inventory, addedItems):
+    for item in range(len(addedItems)):
+        inventory.setdefault(addedItems[item],0)
+        inventory[addedItems[item]]=inventory[addedItems[item]]+1
+    print("Inventory:")
+    item_total = 0
+    for k, v in inventory.items():
+        print(str(v) + " " + str(k))
+        item_total = item_total + v
+    print("Total number of items: " + str(item_total))
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+addToInventory(inv, dragonLoot)
