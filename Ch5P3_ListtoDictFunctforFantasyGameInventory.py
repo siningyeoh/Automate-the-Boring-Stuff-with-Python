30b6006189e188d8c747b2686b506a3aa44c372f
siningyeoh <75380967+siningyeoh@users.noreply.github.com>
2021-08-13 17:07:46 +0100

    Update Ch5P3_ListtoDictFunctforFantasyGameInventory.py

--- Ch5P3_ListtoDictFunctforFantasyGameInventory.py
+++ Ch5P3_ListtoDictFunctforFantasyGameInventory.py
@@ -2,6 +2,9 @@ def addToInventory(inventory, addedItems):
     for item in addedItems:
         inventory.setdefault(item,0)
         inventory[item] += 1
+    return inventory
+
+def displayInventory(inventory):
     print("Inventory:")
     item_total = 0
     for k, v in inventory.items():
@@ -11,4 +14,5 @@ def addToInventory(inventory, addedItems):
 
 inv = {'gold coin': 42, 'rope': 1}
 dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
-addToInventory(inv, dragonLoot)
+inv=addToInventory(inv, dragonLoot)
+displayInventory(inv)
